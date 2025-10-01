39f4446a478bbed79693a0b991a7bc3f3b1da492
AMansour5 <ahmedmonsour5@icloud.com>
2025-10-02 01:54:06 +0300

    Wheelchair Lidar simulated

--- src/my_robot_bringup/launch/my_wheelchair.launch.py
+++ src/my_robot_bringup/launch/my_wheelchair.launch.py
@@ -78,10 +78,12 @@ def generate_launch_description():
         executable="parameter_bridge",
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
-            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU"
+            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
+            "/lidar@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
         ],
         remappings=[
             ('/imu', '/imu/out'),
+            ('/lidar', '/lidar/out'),
         ]
     )
 
