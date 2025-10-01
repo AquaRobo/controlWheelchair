fe4aa7d4e001dbfae1a0342cd779dd1ed127ff2c
AMansour5 <ahmedmonsour5@icloud.com>
2025-10-01 23:39:40 +0300

    wheelchair simulated in gazebo

--- src/my_robot_bringup/launch/my_wheelchair.launch.py
+++ src/my_robot_bringup/launch/my_wheelchair.launch.py
@@ -1,37 +1,58 @@
 import os
 from ament_index_python.packages import get_package_share_directory, get_package_share_path
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
 
+    # Paths
     urdf_path = os.path.join(get_package_share_path('my_robot_description'),
                              'urdf', 'my_wheelchair.urdf.xacro')
     rviz_config_path = os.path.join(get_package_share_path('my_robot_description'),
                                     'rviz', 'wheelchair_config.rviz')
+    controllers_yaml = os.path.join(get_package_share_path('my_robot_bringup'),
+                                    'config', 'my_wheelchair_controller.yaml')
     
     robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        output="screen",
         parameters=[{'robot_description': robot_description, "use_sim_time": True}]
     )
 
-    joint_state_publisher_gui_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui"
+    ros2_control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[controllers_yaml, {'robot_description': robot_description}],
+        output='screen',
+        name='controller_manager'
+    )
+
+    joint_state_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+        output='screen'
+    )
+
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_drive_controller'],
+        output='screen'
     )
 
     rviz2_node = Node(
         package="rviz2",
         executable="rviz2",
+        output="screen",
         arguments=['-d', rviz_config_path]
     )
 
@@ -52,11 +73,25 @@ def generate_launch_description():
                    "-name", "my_wheelchair"],
     )
 
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU"
+        ],
+        remappings=[
+            ('/imu', '/imu/out'),
+        ]
+    )
+
     return LaunchDescription([
         robot_state_publisher_node,
-        # joint_state_publisher_gui_node,
+        # ros2_control_node,
+        joint_state_spawner,
+        diff_drive_spawner,
         rviz2_node,
         gazebo,
         gz_spawn_entity,
+        gz_ros2_bridge,
     ])
-
