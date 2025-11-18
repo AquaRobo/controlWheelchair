bef5f79c243389bdfd5f1bc6fffbd1b73bd5f04a
AMansour5 <ahmedmonsour5@icloud.com>
2025-11-18 22:27:55 +0200

    DepthCamera Added

--- src/my_robot_bringup/launch/my_wheelchair.launch.py
+++ src/my_robot_bringup/launch/my_wheelchair.launch.py
@@ -1,12 +1,13 @@
 import os
 from ament_index_python.packages import get_package_share_directory, get_package_share_path
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, RegisterEventHandler, TimerAction
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
+from launch.event_handlers import OnProcessStart
 
 def generate_launch_description():
 
@@ -80,6 +81,10 @@ def generate_launch_description():
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
             "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
             "/lidar@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
+            "/lidar/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
+            "/depth_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
+            "/depth_camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
+            "/depth_camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image",
         ],
         remappings=[
             ('/imu', '/imu/out'),
@@ -87,11 +92,24 @@ def generate_launch_description():
         ]
     )
 
+    delayed_joint_state_spawner = RegisterEventHandler(
+    event_handler=OnProcessStart(
+        target_action=gz_spawn_entity,
+        on_start=[TimerAction(period=8.0, actions=[joint_state_spawner])],
+    ))
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+    event_handler=OnProcessStart(
+        target_action=joint_state_spawner,
+        on_start=[TimerAction(period=3.0, actions=[diff_drive_spawner])],
+    ))
+
+
     return LaunchDescription([
         robot_state_publisher_node,
         # ros2_control_node,
-        joint_state_spawner,
-        diff_drive_spawner,
+        delayed_joint_state_spawner,
+        delayed_diff_drive_spawner,
         rviz2_node,
         gazebo,
         gz_spawn_entity,
