989c4ee6d2e27a4b99744be12f05932e48f2a999
AMansour5 <ahmedmonsour5@icloud.com>
2026-05-15 16:28:01 +0300

    Motors Done

--- src/control/control/services/MotorDriver.py
+++ src/control/control/services/MotorDriver.py
@@ -2,11 +2,14 @@ from zope.interface import implementer
 from control.DTOs.motors import Motors
 from control.interfaces.IMotorDriver import IMotorDriver
 from utils.Dispatcher import Dispatcher
+import struct
+
+START_FRAME = 0xABCD    # uint16 start marker
 
 @implementer(IMotorDriver)
 class MotorDriver:
     def __init__(self):
-        self.commHandler = Dispatcher().get_communication_handler("ESP")
+        self.commHandler = Dispatcher().get_communication_handler("STM")
 
     def drive(self, motors_dict: dict[str, Motors]) -> None:
         motors_speeds = self.__buildMotorsArray(motors_dict)
@@ -21,7 +24,11 @@ class MotorDriver:
         Example:
             data = ["w", motor.current_speed, .....for number of motors]
         """
-        data = ["w"]
+        data = []
         for motor in motors_dict.values():
-            data.extend([float(motor.current_pwm)])
-        return data
+            data.extend([int(motor.current_pwm)])
+        
+        checksum = START_FRAME ^ (data[-1] & 0xFFFF) ^ (data[-2] & 0xFFFF)
+        return struct.pack('<HhhH', START_FRAME, data[-1], data[-2], checksum)
+
+
